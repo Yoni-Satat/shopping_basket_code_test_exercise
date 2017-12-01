84e309e4009523f1c7483328a0a61805d1810da6
Yoni Satat <yoni.satat@gmail.com>
2017-12-01 14:44:35 +0000

    started project

---
+++ app/src/main/java/codeclan/com/shoppingbasket/Item.java
@@ -0,0 +1,8 @@
+package codeclan.com.shoppingbasket;
+
+/**
+ * Created by user on 01/12/2017.
+ */
+
+public class Item {
+}
---
+++ app/src/main/java/codeclan/com/shoppingbasket/ShoppingBasket.java
@@ -0,0 +1,15 @@
+package codeclan.com.shoppingbasket;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 01/12/2017.
+ */
+
+public class ShoppingBasket {
+    private ArrayList<Item> items;
+
+    public ShoppingBasket(){
+        items = new ArrayList<>();
+    }
+}
